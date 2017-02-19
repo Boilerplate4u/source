c7f7032e4c1bd2dc2aa4ea661667603ef539c58f
Boilerplate4U <Boilerplate4U@gmail.com>
2017-02-19 10:28:13 +0100

    kernel 4.9

--- target/linux/ar71xx/do49patches.sh
+++ target/linux/ar71xx/do49patches.sh
@@ -1,1 +1,1 @@
-/home/lars/src/lede/scripts/patch-kernel.sh /home/lars/src/lede/build_dir/target-mips_24kc_musl/linux-ar71xx_mikrotik/linux-4.9.10 /home/lars/src/lede/target/linux/ar71xx/patches-4.9 $1
+~/src/lede/scripts/patch-kernel.sh ~/src/lede/build_dir/target-mips_24kc_musl/linux-ar71xx_mikrotik/linux-4.9.10 ~/src/lede/target/linux/ar71xx/patches-4.9 $1
